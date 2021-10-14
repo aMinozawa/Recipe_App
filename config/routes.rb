ad88f5e23996b9e5f49d2b444e341af1fbf09973
A.Minozawa <a.minozawa@cybermissions.co.jp>
2021-10-14 15:49:44 +0900

    deviseインストール＆名前を登録できるように

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'recipes#index'
   get   "/recipes",       to: "recipes#index"
   get   "/recipes/new",   to: "recipes#new"
   post  "/recipes",       to: "recipes#create"
