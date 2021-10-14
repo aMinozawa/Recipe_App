d072ed668b5a1af5b43b7f23836e64ee7fd9c453
A.Minozawa <a.minozawa@cybermissions.co.jp>
2021-10-14 12:34:25 +0900

    投稿フォームの修正

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,10 @@ class RecipesController < ApplicationController
     @text = "Randing Page"
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   def new
     @recipe = Recipe.new
   end
@@ -11,7 +15,7 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
       flash[:notice] = "レシピを投稿しました。"
-      redirect_to("/recipes/new")
+      redirect_to("/recipes/#{@recipe.id}")
     else
       flash[:alert] = "レシピが登録できませんでした。"
       render("recipes/new")
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get   "/recipes",       to: "recipes#index"
   get   "/recipes/new",   to: "recipes#new"
   post  "/recipes",       to: "recipes#create"
+  get   "/recipes/:id",   to: "recipes#show"
 end
